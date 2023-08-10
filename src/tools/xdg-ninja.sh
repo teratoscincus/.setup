f23e0fca7a6a8821b0ec0667d1eceaa72f53e9e2
Max <teratoscincus@hotmail.com>
2023-08-11 01:40:25 +0200

    Add xdg-ninja setup

---
+++ src/tools/xdg-ninja.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+repo_dest="$XDG_DATA_HOME"/xdg-ninja
+if [[ ! -d "$repo_dest" ]]; then
+	git clone git@github.com:b3nj5m1n/xdg-ninja.git "$repo_dest"
+fi
+
+# Place executable in $PATH
+wrapper="$HOME"/.local/bin/xdg-ninja
+
+cat >"$wrapper" <<EOM
+bash $repo_dest/xdg-ninja.sh  --skip-unsupported
+EOM
+
+chmod +x "$wrapper"
+
+# Clean up
+unset repo_dest wrapper
